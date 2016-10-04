70f4c82529a8cacec394286735abed3576c90039
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-04 17:27:55 +1300

    playing with strings

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            string myStr = "";
+            // string myStr = "My so called \"Life\"\n\n\n hmm";
+            // string myStr = "go to your c:\\ drive";
+            // string myStr = @"go to your c:\ drive";
+            // string myStr = String.Format("{0} = {0} != {1}", "first", "second");
+            // string myStr = String.Format("{0:C}", 123.45);
+            // string myStr = String.Format("{0:N}", 1234567890);
+            // string myStr = String.Format("{0:P}", .123 );
+            string myStr = String.Format("{0:(###) ###-####}", 1234567890);
             Console.WriteLine(myStr);
             Console.ReadLine();
         }
