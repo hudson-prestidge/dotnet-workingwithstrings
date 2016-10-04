3492022775820ab48a6863e28716f215ae00587a
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-04 17:04:24 +1300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string myStr = "";
+            Console.WriteLine(myStr);
+            Console.ReadLine();
+        }
+    }
+}
