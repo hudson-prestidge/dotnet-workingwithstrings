469a50d3e105adaf8439a386826817b8f4de4459
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-04 17:39:29 +1300

    more playing with strings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ConsoleApplication
 {
@@ -13,8 +14,26 @@ namespace ConsoleApplication
             // string myStr = String.Format("{0:C}", 123.45);
             // string myStr = String.Format("{0:N}", 1234567890);
             // string myStr = String.Format("{0:P}", .123 );
-            string myStr = String.Format("{0:(###) ###-####}", 1234567890);
+            // string myStr = String.Format("{0:(###) ###-####}", 1234567890);
+
+            string myStr = " that summer we took threes across the board ";
+            // myStr = myStr.Substring(0,13).Replace(" ", "--").Remove(6, 2);
+
+            myStr = String.Format("length before: {0} - length after: {1}",
+            myStr.Length, myStr.Trim().Length);
+
+            // strings are immutable!
+            // method names all start with uppercase!
+            // don't forget semicolons!
+
+            StringBuilder myString = new StringBuilder();
+
+            for (int i = 0; i < 50; i++){
+                myString.Append(i);
+                myString.Append("--");
+            }
             Console.WriteLine(myStr);
+            Console.WriteLine(myString);
             Console.ReadLine();
         }
     }
